221a3566c0f55588c592838ad8f153d8acb63366
Stefan Markmann <stefan@markmann.net>
2020-12-14 11:22:56 +0100

    fixed hmac keys with leading dash

--- certbot-zerossl.sh
+++ certbot-zerossl.sh
@@ -7,7 +7,14 @@ function parse_eab_credentials()
     PYTHONIOENCODING=utf8
     ZEROSSL_EAB_KID=$(echo $1 | python -c "import sys, json; print(json.load(sys.stdin)['eab_kid'])")
     ZEROSSL_EAB_HMAC_KEY=$(echo $1 | python -c "import sys, json; print(json.load(sys.stdin)['eab_hmac_key'])")
-    CERTBOT_ARGS+=(--eab-kid "$ZEROSSL_EAB_KID" --eab-hmac-key "$ZEROSSL_EAB_HMAC_KEY" --server "https://acme.zerossl.com/v2/DV90")
+    
+    # Check if hmac key starts with dash - this will confuse phython or certbot, see https://github.com/certbot/certbot/issues/7114
+	if  [[ $ZEROSSL_EAB_HMAC_KEY != -* ]];	then
+	    GOOD_KEY_FOUND=true
+	    CERTBOT_ARGS+=(--eab-kid "$ZEROSSL_EAB_KID" --eab-hmac-key "$ZEROSSL_EAB_HMAC_KEY" --server "https://acme.zerossl.com/v2/DV90")
+    else
+        echo "Got hmac key with leading dash, retrying..."
+    fi
 }
 
 while [[ "$#" -gt 0 ]]; do
@@ -33,9 +40,16 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 if [[ -n $ZEROSSL_API_KEY ]]; then
-    parse_eab_credentials $(curl -s -X POST "https://api.zerossl.com/acme/eab-credentials?access_key=$ZEROSSL_API_KEY")
+
+	# Retry until we get one hmac key without leading dash
+    GOOD_KEY_FOUND=false
+    while ! $GOOD_KEY_FOUND; do
+        parse_eab_credentials $(curl -s -X POST "https://api.zerossl.com/acme/eab-credentials?access_key=$ZEROSSL_API_KEY")
+    done
+    
 elif [[ -n $ZEROSSL_EMAIL ]]; then
     parse_eab_credentials $(curl -s https://api.zerossl.com/acme/eab-credentials-email --data "email=$ZEROSSL_EMAIL")
 fi
 
+echo "$(date) Calling: certbot ${CERTBOT_ARGS[@]}"
 certbot ${CERTBOT_ARGS[@]}
