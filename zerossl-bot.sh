a9c6e17018224c9bfc3289be86d6a285f8db22c6
David Stöckl <david.stoeckl@blackbam.at>
2023-10-03 11:00:40 +0200

    fix. missing echo

--- zerossl-bot.sh
+++ zerossl-bot.sh
@@ -40,4 +40,5 @@ elif [[ -n $ZEROSSL_EMAIL ]]; then
     parse_eab_credentials "$(curl -s https://api.zerossl.com/acme/eab-credentials-email --data "email=$ZEROSSL_EMAIL")"
 fi
 
+echo "${CERTBOT_ARGS[@]}"
 certbot "${CERTBOT_ARGS[@]}"
